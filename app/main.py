981bbaa917dbc981fb457b4a23f12088f09de326
maheshkokare <maheshkokare100@gmail.com>
2023-05-31 20:39:43 +0100

    checking if precommit works

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 
@@ -12,5 +11,6 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
 
 # run fastapi
