dd2770248d82a20614cd861e348121667f76c618
maheshkokare <maheshkokare100@gmail.com>
2023-07-16 18:49:19 +0100

    added backup_db task

--- background/scheduler.py
+++ background/scheduler.py
@@ -114,8 +114,8 @@ async def task_update_till_yesterdays_profits():
     await asyncio.gather(*tasks)
 
 
-async def task_close_and_buy_trades_in_next_expiry():
-    logging.info(f"Job task_close_and_buy_trades_in_next_expiry executed at: {datetime.now()}")
+async def task_rollover_to_next_expiry():
+    logging.info(f"Job task_rollover_to_next_expiry executed at: {datetime.now()}")
 
     tasks = []
     for app in base_urls:
@@ -126,14 +126,27 @@ async def task_close_and_buy_trades_in_next_expiry():
     await asyncio.gather(*tasks)
 
 
+async def task_backup_db():
+    logging.info(f"Job task_backup_db executed at: {datetime.now()}")
+
+    tasks = []
+    for app in base_urls:
+        if app == "flaskstockpi":
+            tasks.append(get_api(f"{base_urls[app]}/start_backup"))
+
+    # wait for all tasks to complete
+    await asyncio.gather(*tasks)
+
+
 aiocron.crontab("*/60 * * * *", func=task_interval_test)  # Every 60 minutes
 aiocron.crontab("20 23 * * *", func=task_cron_test)  # At 23:19 every day
+aiocron.crontab("0 2 * * *", func=task_backup_db)  # Every day at 02:00
 aiocron.crontab("0 3 * * 5", func=task_clean_redis)  # Every Friday at 03:00
 aiocron.crontab("0 3 * * *", func=task_update_expiry_list)  # Every day at 03:00
 aiocron.crontab("10 3 * * *", func=task_update_session_token)  # Every day at 03:10
 aiocron.crontab("30 3 * * *", func=task_update_session_token)  # Every day at 03:30
 aiocron.crontab("30 3 * * *", func=task_up_scale_dynos)  # Every day at 03:30
-aiocron.crontab("30 9 * * *", func=task_close_and_buy_trades_in_next_expiry)  # Every day at 9:30
+aiocron.crontab("30 9 * * *", func=task_rollover_to_next_expiry)  # Every day at 9:30
 aiocron.crontab("0 10 * * *", func=task_down_scale_dynos)  # Every day at 10:00
 aiocron.crontab("5 10 * * *", func=task_update_till_yesterdays_profits)  # Every day at 10:05
 
