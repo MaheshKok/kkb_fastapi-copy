a521e6efcb43c310ea9e8678a8ee41552a853214
maheshkokare <maheshkokare100@gmail.com>
2023-10-24 00:10:40 +0100

    added migration for cfd_strategy table
    Signed-off-by: maheshkokare <maheshkokare100@gmail.com>

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -1,5 +1,6 @@
 # noqa: F401
 from app.database.models.broker import BrokerModel
+from app.database.models.cfd_strategy import CFDStrategyModel
 from app.database.models.daily_profit import DailyProfitModel
 from app.database.models.strategy import StrategyModel
 from app.database.models.take_away_profit import TakeAwayProfitModel
