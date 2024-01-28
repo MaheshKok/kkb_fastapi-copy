56e18f751ffaf3d52751cf4ddb78c150a5541182
maheshkokare <maheshkokare100@gmail.com>
2024-01-28 05:01:20 +0000

    untested code in rollover_to_next_expiry.py
    Signed-off-by: maheshkokare <maheshkokare100@gmail.com>

--- cron/rollover_to_next_expiry.py
+++ cron/rollover_to_next_expiry.py
@@ -2,6 +2,8 @@ import asyncio
 import json
 import logging
 from datetime import datetime
+
+# from datetime import datetime
 from typing import List
 
 from httpx import AsyncClient
@@ -14,17 +16,39 @@ from app.database.base import get_db_url
 from app.database.base import get_redis_client
 from app.database.models import StrategyModel
 from app.database.session_manager.db_session import Database
+from app.schemas.enums import InstrumentTypeEnum
+from app.schemas.enums import PositionEnum
+from app.schemas.enums import SignalTypeEnum
 from app.schemas.strategy import StrategySchema
 from app.schemas.trade import RedisTradeSchema
 from app.schemas.trade import SignalPayloadSchema
 from app.tasks.tasks import task_entry_trade
 from app.tasks.tasks import task_exit_trade
 from app.tasks.utils import get_future_price
-from app.utils.constants import SYMBOL
+from app.tasks.utils import get_monthly_expiry_date_from_redis
 from app.utils.constants import OptionType
 
 
-# TODO: test this
+def get_action(strategy_schema: StrategySchema, trade_model: RedisTradeSchema):
+    if strategy_schema.instrument_type == InstrumentTypeEnum.OPTIDX:
+        if strategy_schema.position == PositionEnum.LONG:
+            if trade_model.option_type == OptionType.CE:
+                return SignalTypeEnum.BUY
+            else:
+                return SignalTypeEnum.SELL
+        else:
+            if trade_model.option_type == OptionType.CE:
+                return SignalTypeEnum.SELL
+            else:
+                return SignalTypeEnum.BUY
+    else:
+        if trade_model.quantity > 0:
+            return SignalTypeEnum.BUY
+        else:
+            return SignalTypeEnum.SELL
+
+
+# TODO: Test this function
 async def rollover_to_next_expiry():
     config = get_config()
     Database.init(get_db_url(config))
@@ -44,32 +68,34 @@ async def rollover_to_next_expiry():
         for strategy_model in strategy_models:
             strategy_schema = StrategySchema.model_validate(strategy_model)
 
-            (
-                current_expiry,
-                next_expiry,
-                todays_expiry,
-            ) = await get_current_and_next_expiry_from_redis(async_redis_client, strategy_schema)
+            if strategy_schema.instrument_type == InstrumentTypeEnum.OPTIDX:
+                (
+                    current_expiry,
+                    next_expiry,
+                    todays_expiry,
+                ) = await get_current_and_next_expiry_from_redis(
+                    async_redis_client, strategy_schema
+                )
 
-            if not todays_expiry:
-                continue
+                if not todays_expiry:
+                    continue
+            else:
+                (
+                    current_expiry,
+                    next_expiry,
+                    todays_expiry,
+                ) = await get_monthly_expiry_date_from_redis(
+                    async_redis_client=async_redis_client,
+                    instrument_type=InstrumentTypeEnum.FUTIDX,
+                    symbol=strategy_schema.symbol,
+                )
+
+                if not todays_expiry:
+                    continue
 
             # fetch redis trades
             redis_strategy_data = await async_redis_client.hgetall(str(strategy_schema.id))
-
-            live_trade_option_type_list = [
-                OptionType.CE if OptionType.CE in key else OptionType.PE
-                for key in redis_strategy_data.keys()
-            ]
-            if live_trade_option_type_list:
-                live_trade_option_type = live_trade_option_type_list[0]
-
-            redis_hash = f"{current_expiry} {live_trade_option_type}"
-            if redis_hash not in redis_strategy_data:
-                logging.info(
-                    f"No trades found for {strategy_schema.id} {strategy_schema.symbol} on {redis_hash}"
-                )
-                continue
-
+            redis_hash = redis_strategy_data.keys()[0]
             exiting_trades_json_list = json.loads(redis_strategy_data[redis_hash])
             logging.info(f"Existing total: {len(exiting_trades_json_list)} trades to be closed")
             redis_trade_schema_list = TypeAdapter(List[RedisTradeSchema]).validate_python(
@@ -80,46 +106,44 @@ async def rollover_to_next_expiry():
                 future_entry_price_received = future_price_cache[strategy_schema.symbol]
             else:
                 future_entry_price_received = await get_future_price(
-                    async_redis_client, strategy_schema
+                    async_redis_client=async_redis_client,
+                    strategy_schema=strategy_schema,
+                    expiry_date=current_expiry,
                 )
                 future_price_cache[strategy_schema.symbol] = future_entry_price_received
 
-            # payload should be like closing live trades so option_type should be opposite to that of redis trades
-            payload = {
-                "quantity": sum([trade.quantity for trade in redis_trade_schema_list]),
+            trade_schema = redis_trade_schema_list[0]
+            signal_payload = {
                 "future_entry_price_received": future_entry_price_received,
                 "strategy_id": strategy_schema.id,
-                "option_type": OptionType.CE
-                if live_trade_option_type == OptionType.CE
-                else OptionType.PE,
-                "position": strategy_schema.position,
-                "received_at": datetime.now().isoformat(),
-                "premium": 400.0 if strategy_schema.symbol == SYMBOL.BANKNIFTY else 200.0,
-                "expiry": next_expiry,
+                "action": get_action(strategy_schema, trade_schema),
+                "received_at": str(datetime.utcnow().isoformat()),
             }
 
-            signal_payload_schema = SignalPayloadSchema(**payload)
-            exit_task = asyncio.create_task(
-                task_exit_trade(
-                    signal_payload_schema=signal_payload_schema,
-                    async_redis_client=async_redis_client,
-                    strategy_schema=strategy_schema,
-                    async_httpx_client=async_httpx_client,
-                    redis_strategy_key_hash=f"{strategy_schema.id} {redis_hash}",
-                    redis_trade_schema_list=redis_trade_schema_list,
-                )
+            signal_payload_schema = SignalPayloadSchema(**signal_payload)
+            kwargs = {
+                "signal_payload_schema": signal_payload_schema,
+                "strategy_schema": strategy_schema,
+                "async_redis_client": async_redis_client,
+                "async_httpx_client": async_httpx_client,
+                "only_futures": strategy_schema.instrument_type == InstrumentTypeEnum.FUTIDX,
+            }
+
+            sell_task = await task_exit_trade(
+                **kwargs,
+                redis_hash=redis_hash,
+                expiry_date=current_expiry,
+                redis_trade_schema_list=redis_trade_schema_list,
             )
+            tasks.append(sell_task)
+
+            signal_payload_schema.expiry = next_expiry
             buy_task = asyncio.create_task(
                 task_entry_trade(
-                    signal_payload_schema=signal_payload_schema,
-                    async_redis_client=async_redis_client,
-                    strategy_schema=strategy_schema,
-                    async_httpx_client=async_httpx_client,
+                    **kwargs,
                 )
             )
-            tasks.append(exit_task)
             tasks.append(buy_task)
-
         await asyncio.gather(*tasks)
 
 
