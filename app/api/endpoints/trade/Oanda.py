c4937d3a836d6239174e96832e52dbc06336437f
maheshkokare <maheshkokare100@gmail.com>
2024-01-30 01:55:17 +0000

    added more logging to Oanda.py
    Signed-off-by: maheshkokare <maheshkokare100@gmail.com>

--- app/api/endpoints/trade/Oanda.py
+++ app/api/endpoints/trade/Oanda.py
@@ -1,3 +1,7 @@
+import logging
+import time
+from pprint import pprint
+
 from fastapi import APIRouter
 from fastapi import Depends
 from oandapyV20.contrib.requests import MarketOrderRequest
@@ -29,21 +33,31 @@ async def post_cfd(
     cfd_payload_schema: CFDPayloadSchema,
     cfd_strategy_schema: CFDStrategySchema = Depends(get_cfd_strategy_schema),
 ):
+    start_time = time.perf_counter()
+    demo_or_live = "DEMO" if cfd_strategy_schema.is_demo else "LIVE"
+
+    logging.info(
+        f"[ {demo_or_live} {cfd_strategy_schema.instrument} ] : signal [ {cfd_payload_schema.direction} ] received"
+    )
+
     access_token = "c1a1da5b257e3eb61082d88d6c41108d-3c1a484c1cf2b8ee215bef4e36807aad"
     client = AsyncAPI(access_token=access_token)
-    demo_or_live = "DEMO" if cfd_strategy_schema.is_demo else "LIVE"
 
     account_id = cfd_payload_schema.account_id
     trades = await client.request(TradesList(account_id=account_id))
 
     profit_or_loss = 0
     if trades["trades"]:
+        logging.info(f"[ {demo_or_live} {cfd_strategy_schema}] trades to be closed")
         # exit existing trades
         close_trade_request = TradeCloseRequest()
         close_trade_response = await client.request(TradeClose(close_trade_request.data))
         if "orderFillTransaction" in close_trade_response.response:
             trades_closed = close_trade_response["orderFillTransaction"]["trades_closed"]
             profit_or_loss = trades_closed[0]["realizedPL"]
+            logging.info(
+                f"[ {demo_or_live} {cfd_strategy_schema}] trades closed with profit [ {profit_or_loss} ]"
+            )
             await update_capital_funds(
                 cfd_strategy_schema=cfd_strategy_schema,
                 demo_or_live=demo_or_live,
@@ -53,10 +67,24 @@ async def post_cfd(
     lots_to_open, update_profit_or_loss_in_db = get_lots_to_trade_and_profit_or_loss(
         0, cfd_strategy_schema, profit_or_loss
     )
+    logging.info(
+        f"[ {demo_or_live} {cfd_strategy_schema}] [ {lots_to_open} ] trades to be opened"
+    )
     # buy new trades
     market_order_request = MarketOrderRequest(
         instrument=cfd_strategy_schema.instrument,
         units=lots_to_open,
     )
     response = await client.request(OrderCreate(account_id, data=market_order_request.data))
+    long_or_short = "LONG" if cfd_payload_schema.direction == "buy" else "SHORT"
+    if "orderFillTransaction" in response:
+        msg = f"[ {demo_or_live} {cfd_strategy_schema.instrument} ] : successfully [ {long_or_short}  {lots_to_open} ] trades."
+        logging.info(msg)
+    else:
+        msg = f"[ {demo_or_live} {cfd_strategy_schema.instrument} ] : Error occured while [ {long_or_short}  {lots_to_open} ] trades"
+        logging.error(msg)
+        pprint(response)
+
+    process_time = time.perf_counter() - start_time
+    logging.info(f" API: [ post_oanda_cfd ] request processing time: {process_time} seconds")
     return response
