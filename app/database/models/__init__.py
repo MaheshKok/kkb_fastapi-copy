ad1798766352f9505bbe8b27490f16a5a53e83bb
maheshkokare <maheshkokare100@gmail.com>
2024-04-22 01:24:56 +0100

    models to db models
    Signed-off-by: maheshkokare <maheshkokare100@gmail.com>

--- app/database/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# noqa: F401
-from app.database.models.broker import BrokerModel
-from app.database.models.cfd_strategy import CFDStrategyModel
-from app.database.models.daily_profit import DailyProfitModel
-from app.database.models.strategy import StrategyModel
-from app.database.models.trade import TradeModel
-from app.database.models.user import User
